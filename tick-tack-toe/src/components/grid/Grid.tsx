66c35270f80e199549d2d1c46f799dc10854e74e
Max Xue <maxxue1512@gmail.com>
2022-04-27 14:39:54 +1000

    added cells and changing board state functionality

--- tick-tack-toe/src/components/grid/Grid.tsx
+++ tick-tack-toe/src/components/grid/Grid.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './grid.css';
+import Tile from '../tile/Tile'
 
 export default function Grid() {
-
-    
+   
+  const [board, setBoard] = useState([['-','-','-'],['-','-','-'],['-','-','-']]);
+  const [o_turn, setO_turn] = useState(true);
 
 
   return (
-    <div className='grid-container'>Grid
-        <div className="tiles-container">
+    <div className='grid-container'>
+        
+        {board.map((board_state, row_index) => (
+          board_state.map((tile_state, col_index) => (
+            <Tile 
+              key={`${row_index} ${col_index}`}
+              o_turn={o_turn}
+              setO_turn={setO_turn}
+              board={board}
+              setBoard={setBoard}
+              row={row_index}
+              col={col_index}
+            />
 
-        </div>
+          ))
+        ))}
+        
     </div>
   )
 }
--- tick-tack-toe/src/components/tile/Tile.tsx
+++ tick-tack-toe/src/components/tile/Tile.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import './tile.css';
+import { MouseEvent } from 'react';
 
-export default function Tile() {
+interface boardState {
+  o_turn: boolean;
+  setO_turn: (o_turn: boolean) => void;
+  board: string[][];
+  setBoard: (board: string[][]) => void;
+  row: number;
+  col: number;
+}
+
+
+export default function Tile({ o_turn, setO_turn, board, setBoard, row, col }: boardState) {
+
+  function board_change(event: MouseEvent) {
+    if (board[row][col] === '-') {
+      if (o_turn === true) {
+        const new_board = board;
+        new_board[row][col] = 'o';
+        setBoard(new_board);
+        setO_turn(false)
+      } else {
+        const new_board = board;
+        new_board[row][col] = 'x';
+        setBoard(new_board);
+        setO_turn(true)
+      }
+    } 
+  }
+
+  
+
+
   return (
-    <div>Tile</div>
+    <div className="tiles-container" onClick={board_change}>{board[row][col]}</div>
   )
 }
